181585b937d5261ac40f4f183881621d920fb28d
Miguel Arcos <miguel.arcos.trader@gmail.com>
2025-07-29 12:54:35 -0500

    :sparkles: Add swagger & open api doc

--- src/main/java/com/marcos/products_service/dto/ProductDTO.java
+++ src/main/java/com/marcos/products_service/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package com.marcos.products_service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
@@ -9,14 +10,23 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(description = "Data Transfer Object for product operations")
 public class ProductDTO {
 
+    @Schema(description = "Name of the product",
+            example = "Premium Wireless Headphones",
+            requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank(message = "Product name cannot be blank")
     private String name;
 
+    @Schema(description = "Price of the product in USD",
+            example = "199.99",
+            requiredMode = Schema.RequiredMode.REQUIRED)
     @Positive(message = "Product price must be positive")
     private double price;
 
+    @Schema(description = "Detailed description of the product",
+            example = "Noise-cancelling wireless headphones with 30-hour battery life")
     private String description;
 
     public ProductDTO(String name, double price, String description) {
@@ -24,5 +34,4 @@ public class ProductDTO {
         this.price = price;
         this.description = description;
     }
-
 }
